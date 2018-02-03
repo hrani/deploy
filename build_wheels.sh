a05cbb41a1d129a0ffcf285b49584b9f1e48fe33
Dilawar Singh <dilawars@ncbs.res.in>
2018-02-03 21:42:21 +0530

    specify pypi compatible version from command line.

--- build_wheels.sh
+++ build_wheels.sh
@@ -28,6 +28,7 @@ for PYDIR in /opt/python/cp27-cp27m/ /opt/python/cp34-cp34m/ /opt/python/cp36-cp
         $PYTHON -m pip install numpy
         $CMAKE -DPYTHON_EXECUTABLE=$PYTHON  \
             -DGSL_STATIC_LIBRARIES=$GSL_STATIC_LIBS \
+            -DVERSION_MOOSE=3.2.0rc1 \
             ${MOOSE_SOURCE_DIR}
         make -j4
         
