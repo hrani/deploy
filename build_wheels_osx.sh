99ffd01269b307a49646a1da857d672c8bed29b7
Dilawar Singh <dilawars@ncbs.res.in>
2018-02-06 13:29:33 +0530

    Solution to failing installation of setuptools on El Capitan.

--- build_wheels_osx.sh
+++ build_wheels_osx.sh
@@ -5,7 +5,7 @@ set -x
 brew install gsl 
 sudo easy_install pip --upgrade || echo "Failed to upgrade pip"
 
-sudo pip install setuptools --upgrade 
+sudo pip install setuptools --upgrade --user=python
 pip install delocate --upgrade --user
 pip install twine  --upgrade --user
 
