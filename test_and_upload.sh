b60496f5e9a5fb8228b80592aa7b765f64f4cb16
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-03 09:59:24 +0530

    Test and upload script.

---
+++ test_and_upload.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: test_and_upload.sh
+#
+#         USAGE: ./test_and_upload.sh
+#
+#   DESCRIPTION: Test each wheels and upload.
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Dilawar Singh (), dilawars@ncbs.res.in
+#  ORGANIZATION: NCBS Bangalore
+#       CREATED: Saturday 03 March 2018 09:56:20  IST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                                  # Treat unset variables as an error
+
