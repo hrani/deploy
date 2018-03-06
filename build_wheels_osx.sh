5ab86704ece9e428a3022b1ae63f8274e6b346ad
Dilawar Singh <dilawars@ncbs.res.in>
2018-03-06 17:41:05 +0530

    For OSX we need setuptools.

--- build_wheels_osx.sh
+++ build_wheels_osx.sh
@@ -8,34 +8,43 @@ VERSION=$(cat ./VERSION)
 brew install gsl 
 sudo /usr/bin/easy_install virtualenv
 # setup virtualenv
-python -m virtualenv -p /usr/bin/python $HOME/python2
+virtualenv -p /usr/bin/python $HOME/python2
 source $HOME/python2/bin/activate
 
 pip install setuptools --upgrade
+pip install wheel --upgrade
 pip install numpy --upgrade
 pip install delocate --upgrade 
 pip install twine  --upgrade 
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-curl -sL -O https://github.com/BhallaLab/moose-core/archive/${RELEASE}.tar.gz
-tar xvf ${RELEASE}.tar.gz
+MOOSE_SOURCE_DIR=`pwd`/moose-core
 
-MOOSE_SOURCE_DIR=`pwd`/moose-core-$VERSION
-mkdir -p $MOOSE_SOURCE_DIR/_build
+if [ ! -d $MOOSE_SOURCE_DIR ]; then
+	git clone https://github.com/BhallaLab/moose-core -b $RELEASE --depth 10
+fi
+cd moose-core && git pull
+WHEELHOUSE=$HOME/wheelhouse
+mkdir -p $WHEELHOUSE
 
 PLATFORM=$(python -c "import distutils.util; print(distutils.util.get_platform())")
-echo "Building wheel for $PLATFORM"
-cd $MOOSE_SOURCE_DIR/_build
-cmake -DVERSION_MOOSE=$VERSION -DDEBUG=OFF -DCMAKE_RELEASE_TYPE=Release ..
-make -j3 
-cd python && python setup.cmake.py bdist_wheel -p $PLATFORM  
-
-# Now fix the wheel using delocate.
-rm -rf $HOME/wheelhouse
-mkdir -p $HOME/wheelhouse
-cd $MOOSE_SOURCE_DIR/_build/python/ && delocate-wheel -w $HOME/wheelhouse -v dist/*.whl
-ls $HOME/wheelhouse/pymoose*.whl
+( 
+	cd $MOOSE_SOURCE_DIR
+	mkdir -p _build && cd _build
+	echo "Building wheel for $PLATFORM"
+	cmake -DVERSION_MOOSE=$VERSION -DDEBUG=OFF -DCMAKE_RELEASE_TYPE=Release ..
+	make -j3 
+	( 
+		cd python 
+		ls *.py
+		sed "s/from distutils.*setup/from setuptools import setup/g" setup.cmake.py > setup.wheel.py
+		python setup.wheel.py bdist_wheel -p $PLATFORM 
+		# Now fix the wheel using delocate.
+		delocate-wheel -w $WHEELHOUSE -v dist/*.whl
+	)
+	ls $WHEELHOUSE/pymoose*.whl
+)
 
 if [ -n "$PYPI_PASSWORD" ]; then
     python -m twine upload -u bhallalab -p $PYPI_PASSWORD $HOME/wheelhouse/pymoose*.whl
