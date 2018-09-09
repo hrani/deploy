38cdbbd52d9e10591adf950d2285c920cfbd4d63
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-09 16:44:37 +0530

    Build packages using osc.

--- build_linux_package_using_osc.sh
+++ build_linux_package_using_osc.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
+set -x 
+set -e 
+
 echo "Getting supported repository."
 if [ ! -d home:moose ]; then
     osc co home:moose
 fi
-cd home:moose
-osc update
+
+function buildRepo {
+    echo "  Building for $1" 
+    osc build "$1"
+}
+
+(
+    cd home:moose/pymoose
+    osc update
+    REPOS=$(osc repositories | cut -d' ' -f 1)
+    for _repo in $REPOS; do
+        buildRepo $_repo 
+    done
+)
