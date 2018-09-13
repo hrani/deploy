bb3cd8f2e7711c858ea4598b364aff120a924334
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-13 17:19:44 +0530

    This worked fine locally. Dependencies are missing.

--- TestPackages/run.sh
+++ TestPackages/run.sh
@@ -3,9 +3,8 @@
 for _os in centos; do
     DOCKERFILE=./Dockerfile.${_os}
     docker build \
-        --no-cache \
-        --tag moose-package:${_os}
-        --build-arg HTTP_PROXY=proxy.ncbs.res.in:3128 \
-        --build-arg HTTPS_PROXY=proxy.ncbs.res.in:3128 \
-        -f $DOCKERFILE
+        --tag moose-package:${_os} \
+        --build-arg http_proxy="http://proxy.ncbs.res.in:3128/" \
+        --build-arg https_proxy="http://proxy.ncbs.res.in:3128/" \
+        -f $DOCKERFILE .
 done
