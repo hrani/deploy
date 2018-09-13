508785bd86a5675aa2b26d5e5948832ba6764999
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-13 17:39:10 +0530

    Run fresh every time.

--- TestPackages/run.sh
+++ TestPackages/run.sh
@@ -3,6 +3,7 @@
 for _os in centos; do
     DOCKERFILE=./Dockerfile.${_os}
     docker build \
+        --no-cache \
         --tag moose-package:${_os} \
         --build-arg http_proxy="http://proxy.ncbs.res.in:3128/" \
         --build-arg https_proxy="http://proxy.ncbs.res.in:3128/" \
