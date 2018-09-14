b47b5a3e3d992521b977c6cf73b4db9c7e28e1de
Dilawar Singh <dilawars@ncbs.res.in>
2018-09-14 10:42:09 +0530

    Added debian file.

--- TestPackages/run.sh
+++ TestPackages/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
+set -e
 
-for _os in debian centon; do
+for _os in debian centos; do
     DOCKERFILE=./Dockerfile.${_os}
     docker build --tag moose-package:${_os} -f $DOCKERFILE .
 done
