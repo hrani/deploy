277c84329971a879e7bc271522c38fe94c2972f2
HarshaRani <ranishashi@gmail.com>
2022-04-23 11:13:44 +0530

    test

--- build_wheels_linux.sh
+++ build_wheels_linux.sh
@@ -2,10 +2,10 @@
 
 
 # upload to PYPI.
-    echo "check token",$PYPI_API_TOKEN
+    echo "check token...",$PYPI_API_TOKEN
     if [ -n "$PYPI_API_TOKEN" ]; then
 	echo "pypi token is set"
     else
         echo "PYPI password is not set"
     fi
-done
+
