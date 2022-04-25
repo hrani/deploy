90a500e2da941a07ad1e20b20caa0b77a2d1f02a
HarshaRani <ranishashi@gmail.com>
2022-04-25 15:00:03 +0530

    Update build_wheels_linux.sh

--- build_wheels_linux.sh
+++ build_wheels_linux.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 echo "here in the build_wheel ",$PYPI_API_TOKEN
-PYPI_API_TOKEN: ${{ secrets.PYPI_API_TOKEN }}
 # upload to PYPI.
     echo "check token...",$PYPI_API_TOKEN
     if [ -n "$PYPI_API_TOKEN" ]; then
